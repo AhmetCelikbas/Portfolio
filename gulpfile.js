82284ce08162bdf906cc2b8d8f2d1ac42f6e0095
Ahmet Celikbas <celikbas.ahmet@gmail.com>
2016-10-17 09:50:08 +0200

    mise à jour gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,22 @@
-var gulp = require('gulp');
+// including plugins
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    minifyCss = require("gulp-minify-css");
 
 
-var sass = require('gulp-sass');
+// task
+gulp.task('compile-sass', function () {
+    gulp.src('./assets/scss/styles.scss') // path to your file
+    .pipe(sass())
+    .pipe(gulp.dest('./assets/css/'));
+});
+
+
+
+
+// task
+gulp.task('minify-css', function () {
+    gulp.src('./assets/css/styles.css') // path to your file
+    .pipe(minifyCss())
+    .pipe(gulp.dest('path/to/destination'));
+});
