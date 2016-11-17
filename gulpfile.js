1c6153dd6755f1ebbf2f45289922e933b364ea4d
Ahmet Celikbas <celikbas.ahmet@gmail.com>
2016-11-17 18:57:50 +0100

    regroupement des taches gulp en une seule tache

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,11 @@ var gulp = require('gulp'),
 
 
 // task
-gulp.task('compile-sass', function () {
+gulp.task('default', function () {
     gulp.src('./assets/scss/styles.scss') // path to your file
     .pipe(sass())
-    .pipe(gulp.dest('./assets/css/'));
-});
-
-
-
-
-// task
-gulp.task('minify-css', function () {
-    gulp.src('./assets/css/styles.css') // path to your file
+    .pipe(gulp.dest('./assets/css/'))
     .pipe(minifyCss())
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('./assets/css/'));
 });
-
-
-//  Task
-gulp.task('default', ['compile-sass', 'minify-css']);
